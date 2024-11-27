4c8b371f71b811bb2cfa60df2ddd000608bf49ad
Tomas Macieira <tmacieira.tm@gmail.com>
2024-11-27 15:58:56 +0000

    feat: implemented setsockopt

--- player.c
+++ player.c
@@ -17,6 +17,57 @@
 #include <netdb.h>
 #include <sys/select.h>
 
+#define DEFAULT_IP "127.0.0.1"
+#define DEFAULT_PORT "58015"
+
+void parseArguments(int argc, char *argv[], char **GSIP, char **GSPORT) {
+    if (argc == 5) { // Both IP and Port specified
+        *GSIP = argv[2];
+        *GSPORT = argv[4];
+    } else if (argc == 3) {
+        if (strcmp(argv[1], "-n") == 0) { // Port omitted
+            *GSIP = argv[2];
+            *GSPORT = DEFAULT_PORT;
+        } else if (strcmp(argv[1], "-p") == 0) { // IP omitted
+            *GSIP = DEFAULT_IP;
+            *GSPORT = argv[2];
+        }
+    } else if (argc == 1) { // No arguments specified
+        *GSIP = DEFAULT_IP;
+        *GSPORT = DEFAULT_PORT;
+    } else {
+        fprintf(stderr, "[ERR]: INITIAL INPUT HAS WRONG FORMAT\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int createUDPSocket(const char *GSIP, const char *GSPORT, struct addrinfo **res) {
+    int udp_fd, errcode;
+    struct addrinfo hints;
+
+    // Create UDP socket
+    udp_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (udp_fd == -1) {
+        fprintf(stderr, "[ERR]: Couldn't create UDP socket\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Prepare hints for address resolution
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;         // IPv4
+    hints.ai_socktype = SOCK_DGRAM;    // UDP socket
+
+    // Get address info
+    errcode = getaddrinfo(GSIP, GSPORT, &hints, res);
+    if (errcode != 0) {
+        fprintf(stderr, "[ERR]: Couldn't get address info: %s\n", gai_strerror(errcode));
+        close(udp_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    return udp_fd; // Return the socket file descriptor
+}
+
 int getCommand(char* command) {
     if (strcmp(command, "start") == 0)      { return 1;}      // Start new game
     else if (strcmp(command, "try") == 0)   { return 2;}      // try
@@ -35,6 +86,9 @@ void startCommand(char input[], int fd, struct addrinfo *res) {
     ssize_t n;
     socklen_t addrlen;
     struct sockaddr_in addr;
+    struct timeval timeout;
+    timeout.tv_sec = 2;     // 2 seconds
+    timeout.tv_usec = 0;
     char CMD[6];
     char PLID [7];
     char TIME [4];
@@ -47,31 +101,42 @@ void startCommand(char input[], int fd, struct addrinfo *res) {
     // Player ID is a 6 digit number
     if (strlen(PLID) != 6 || strspn(PLID, "0123456789") != 6) {
         fprintf(stderr, "[ERR]: Invalid PLID: %s\n", PLID);
-        exit(1);
+        return;
     }
 
     // Play time is a 3 digit number between 1 and 600
     int play_time = atoi(TIME);
     if (play_time < 1 || play_time > 600) {
         fprintf(stderr, "[ERR]: Invalid time: %s\n", TIME);
-        exit(1);
+        return;
     }
-
-    //printf("%s\n", CMD);
-    //printf("%s\n", PLID);
-    //printf("%s\n", TIME);
 
     // Message format has to end with \n
     MSG[15] = '\n'; 
-
     snprintf(MSG, sizeof(MSG), "SNG %s %s\n", PLID, TIME);
 
+    if (setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0) {
+        fprintf(stderr, "[ERR]: setsockopt failed\n");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+    //printf("%s", MSG);
     n=sendto(fd,MSG,strlen(MSG),0,res->ai_addr,res->ai_addrlen);
     if(n==-1) /*error*/ exit(1);
 
     addrlen=sizeof(addr);
     n=recvfrom(fd,buffer,128,0, (struct sockaddr*)&addr, &addrlen);
     if(n==-1) /*error*/ exit(1);
+
+    // handle not recieving response
+    while (n < 1) {
+        printf("entrou\n");
+        n=sendto(fd,MSG,strlen(MSG),0,res->ai_addr,res->ai_addrlen);
+        if(n==-1) /*error*/ exit(1);
+        n=recvfrom(fd,buffer,128,0, (struct sockaddr*)&addr, &addrlen);
+        if(n==-1) /*error*/ exit(1);
+
+    }
     write(1,buffer,n);
 }
 
@@ -85,43 +150,13 @@ int main(int argc, char *argv[]) {
     char input[128];
 
     int udp_fd,errcode;
-    struct addrinfo hints,*res;
-
-    if (argc == 5){                                     // every argument was specified
-        GSIP = argv[2];
-        GSPORT = argv[4];
-    } else if (argc == 3) {                             // only one argument was specified
-        if (strcmp(argv[1], "-n") == 0) {               // port was ommited
-            GSIP = argv[2];
-            GSPORT = "58015";                           // 58000 + group number
-        } if (strcmp(argv[1], "-p") == 0) {             // ip was ommited
-            GSIP = "127.0.0.1";
-            GSPORT = argv[2];
-        }
-    } else if (argc == 1){                              // no argument was specified
-        GSIP = "127.0.0.1";
-        GSPORT = "58015";
-    } else {
-        printf("[ERR]: INITIAL INPUT HAS WRONG FORMAT\n");
-        exit(0);
-    }
-
-    printf("ip: %s\t port: %s\n", GSIP, GSPORT);
+    struct addrinfo *res;
+    
+    parseArguments(argc, argv, &GSIP, &GSPORT);
+    //printf("ip: %s\t port: %s\n", GSIP, GSPORT);
 
     // UDP Socket
-    udp_fd=socket(AF_INET,SOCK_DGRAM,0); 
-    if(udp_fd==-1) {
-        printf("[ERR]: Couldn't create UDP socket\n");
-        exit(1);
-    }
-    memset(&hints,0,sizeof hints);
-    hints.ai_family=AF_INET;                            //IPv4
-    hints.ai_socktype=SOCK_DGRAM;                       //UDP socket
-    errcode=getaddrinfo(GSIP, GSPORT, &hints, &res);
-    if(errcode!=0) {
-        printf("[ERR]: Couldn't get address info\n");
-        exit(1);
-    }
+    udp_fd = createUDPSocket(GSIP, GSPORT, &res);
 
     // main loop
     while(1) {
@@ -133,34 +168,27 @@ int main(int argc, char *argv[]) {
 
         switch (OPCODE) {
             case 1:                                     // start command
-                printf("START NEW GAME\n");
                 startCommand(input, udp_fd, res);
                 break;
             case 2:
-                printf("TRY\n");
                 break;
             case 3:
-                printf("SHOW TRIALS\n");
                 break;
             case 4:
-                printf("SCOREBOARD\n");
                 break;
             case 5:
-                printf("QUIT\n");
                 freeaddrinfo(res);
                 close(udp_fd);
                 exit(1);
             case 6:
-                printf("exit\n");
                 freeaddrinfo(res);
                 close(udp_fd);
                 exit(1);
                 break;
             case 7:
-                printf("DEBUG\n");
                 break;
             default:
-                printf("[ERR]: WRONG FORMAT\n");
+                fprintf(stderr, "[ERR]: WRONG FORMAT\n");
                 break;
         }
     }
